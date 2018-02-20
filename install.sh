1d8efb58b35c831b9a8d4dce25501be2538fece8
yulahuyed <yulahuyed@vpsorg.top>
2018-02-20 14:30:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 if [ ! -d "~/aria2-1.32.0-linux-gnu-64bit-build1" ]
 then
-  cd ~
   wget https://github.com/xzl2021/aria2-static-builds-with-128-threads/releases/download/v1.32.0/aria2-1.32.0-linux-gnu-64bit-build1.tar.bz2
   tar jxvf aria2-1.32.0-linux-gnu-64bit-build1.tar.bz2
 fi
